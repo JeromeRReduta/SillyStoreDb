3b346f2da049683e42c0cedb64428582a33d9ed2
JRReduta <jrreduta@yahoo.com>
2026-03-30 03:43:36 -0700

    (change) user seeding added & tested

--- src/infrastructure/psql/seed.ts
+++ src/infrastructure/psql/seed.ts
@@ -1,6 +1,8 @@
 import configs from "../../../SillyStoreCommon/configs/Configs.ts";
 import logger from "../../../SillyStoreCommon/logging/Logger.ts";
-import pg, { type Client } from "pg";
+import pg, { QueryConfig, type Client } from "pg";
+import bcrypt from "bcrypt";
+import { log } from "console";
 
 interface Quantities {
     readonly users: number;
@@ -18,24 +20,55 @@ const quantities: Quantities = {
 
 // TODO: figure out how to make this run - also fix .env.development conn string
 
-async function seedUsers({ users, saltRounds }: Quantities): Promise<void> {}
-async function seedProducts({ products }: Quantities): Promise<void> {}
-async function seedOrders({ ordersPerUser }: Quantities): Promise<void> {}
-async function seedOrdersProducts({
-    users,
-    products,
-    ordersPerUser,
-}: Quantities): Promise<void> {}
+async function seedUsers(
+    db: Client,
+    { users, saltRounds }: Quantities,
+): Promise<void> {
+    for (let i = 0; i < users; i++) {
+        const username: string = `user ${i}`;
+        const pwHash: string = await bcrypt.hash(`password ${i}`, saltRounds);
+        const email: string = `email${i}@email.com`;
+        const sql: QueryConfig = {
+            text: `
+            INSERT INTO users (username, pw_hash, email)
+            VALUES ($1, $2, $3)
+            RETURNING *
+            `,
+            values: [username, pwHash, email],
+        };
+        logger.debug("running sql: ", sql);
+        const {
+            id: pgId,
+            username: pgUsername,
+            pw_hash: pgPwHash,
+            email: pgEmail,
+        } = (await db.query(sql)).rows[0];
+        logger.debug("result", { pgId, pgUsername, pgPwHash, pgEmail });
+    }
+}
+
+async function seedProducts(
+    db: Client,
+    { products }: Quantities,
+): Promise<void> {}
+async function seedOrders(
+    db: Client,
+    { ordersPerUser }: Quantities,
+): Promise<void> {}
+async function seedOrdersProducts(
+    db: Client,
+    { users, products, ordersPerUser }: Quantities,
+): Promise<void> {}
 
 async function main(): Promise<void> {
     const db: Client = new pg.Client(configs.db.connectionString);
     logger.debug("Connecting to db...");
     await db.connect();
     logger.debug("Begin seeding...");
-    await seedUsers(quantities);
-    await seedProducts(quantities);
-    await seedOrders(quantities);
-    await seedOrdersProducts(quantities);
+    await seedUsers(db, quantities);
+    await seedProducts(db, quantities);
+    await seedOrders(db, quantities);
+    await seedOrdersProducts(db, quantities);
     logger.debug("Closing db connection...");
     await db.end();
     logger.debug("seed complete! Ending process.");
