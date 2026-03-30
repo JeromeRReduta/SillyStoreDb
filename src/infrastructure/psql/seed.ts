a80962d4a7ab30dda8d789dd6b3d37efd8567605
JRReduta <jrreduta@yahoo.com>
2026-03-30 03:26:28 -0700

    (change) seeding now possible, (refactor) use quantities obj instead of disconnected vars

--- src/infrastructure/psql/seed.ts
+++ src/infrastructure/psql/seed.ts
@@ -2,35 +2,40 @@ import configs from "../../../SillyStoreCommon/configs/Configs.ts";
 import logger from "../../../SillyStoreCommon/logging/Logger.ts";
 import pg, { type Client } from "pg";
 
-const numUsers: number = 5;
-const numProducts: number = 10;
-const numOrdersPerUser: number = 2;
-const numSaltRounds: number = 10;
+interface Quantities {
+    readonly users: number;
+    readonly products: number;
+    readonly ordersPerUser: number;
+    readonly saltRounds: number;
+}
+
+const quantities: Quantities = {
+    users: 5,
+    products: 10,
+    ordersPerUser: 2,
+    saltRounds: 10,
+};
 
 // TODO: figure out how to make this run - also fix .env.development conn string
 
-async function seedUsers(num: number): Promise<void> {}
-async function seedProducts(num: number): Promise<void> {}
-async function seedOrders(numPerUser: number): Promise<void> {}
+async function seedUsers({ users, saltRounds }: Quantities): Promise<void> {}
+async function seedProducts({ products }: Quantities): Promise<void> {}
+async function seedOrders({ ordersPerUser }: Quantities): Promise<void> {}
 async function seedOrdersProducts({
-    numUsers,
-    numProducts,
-    numOrdersPerUser,
-}: {
-    numUsers: number;
-    numProducts: number;
-    numOrdersPerUser: number;
-}): Promise<void> {}
+    users,
+    products,
+    ordersPerUser,
+}: Quantities): Promise<void> {}
 
 async function main(): Promise<void> {
     const db: Client = new pg.Client(configs.db.connectionString);
     logger.debug("Connecting to db...");
     await db.connect();
     logger.debug("Begin seeding...");
-    await seedUsers(numUsers);
-    await seedProducts(numProducts);
-    await seedOrders(numOrdersPerUser);
-    await seedOrdersProducts({ numUsers, numProducts, numOrdersPerUser });
+    await seedUsers(quantities);
+    await seedProducts(quantities);
+    await seedOrders(quantities);
+    await seedOrdersProducts(quantities);
     logger.debug("Closing db connection...");
     await db.end();
     logger.debug("seed complete! Ending process.");
