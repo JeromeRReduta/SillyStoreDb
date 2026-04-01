ee4d64be2101d5ddeb29edfacb24580a28c3e674
JRReduta <jrreduta@yahoo.com>
2026-04-01 03:53:00 -0700

    (change) productrepo - func names

--- src/domain/repositories/ProductRepository.ts
+++ src/domain/repositories/ProductRepository.ts
@@ -2,7 +2,7 @@ import { Order } from "../../../SillyStoreCommon/domain-objects/Order.ts";
 import { Product } from "../../../SillyStoreCommon/domain-objects/Product.ts";
 
 export default interface ProductRepository {
-    getAllProductsAsync(): Promise<Product[] | null>;
-    getProductByIdAsync(id: number): Promise<Product | null>;
+    getAllAsync(): Promise<Product[] | null>;
+    getByIdAsync(id: number): Promise<Product | null>;
     getOrdersIncludingProductAsync(productId: number): Promise<Order[] | null>;
 }
