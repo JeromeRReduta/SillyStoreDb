77a5ff5e85fff4a46ebea7a45e135a3851bdf236
JRReduta <jrreduta@yahoo.com>
2026-04-01 03:46:46 -0700

    (change) productrepo

--- src/domain/repositories/ProductRepository.ts
+++ src/domain/repositories/ProductRepository.ts
@@ -1,3 +1,8 @@
+import { Order } from "../../../SillyStoreCommon/domain-objects/Order.ts";
+import { Product } from "../../../SillyStoreCommon/domain-objects/Product.ts";
+
 export default interface ProductRepository {
-    /** TODO: stub */
+    getAllProductsAsync(): Promise<Product[] | null>;
+    getProductByIdAsync(id: number): Promise<Product | null>;
+    getOrdersIncludingProduct(productId: number): Promise<Order[] | null>;
 }
