0af20415d949f67355a2cc3fc888db2a2855bbd6
JRReduta <jrreduta@yahoo.com>
2026-04-01 03:41:02 -0700

    (change) UserRepo

--- src/domain/repositories/UserRepository.ts
+++ src/domain/repositories/UserRepository.ts
@@ -1,3 +1,21 @@
+import { type token } from "../../../SillyStoreCommon/domain-objects/Token.ts";
+
+export interface getUserAsyncInfo {
+    readonly username: string;
+    readonly pw: string;
+}
+
+export interface createUserAsyncInfo {
+    readonly username: string;
+    readonly pw: string;
+    readonly email: string;
+}
+
 export default interface UserRepository {
-    /** TODO: stub */
+    getUserAsync({ username, pw }: getUserAsyncInfo): Promise<token | null>;
+    createUserAsync({
+        username,
+        pw,
+        email,
+    }: createUserAsyncInfo): Promise<token | null>;
 }
