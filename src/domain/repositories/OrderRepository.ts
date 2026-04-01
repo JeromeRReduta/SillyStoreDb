e0c79e296d21870175b0f11a68c2c3b52ff45311
JRReduta <jrreduta@yahoo.com>
2026-04-01 03:51:59 -0700

    (change) OrderRepo

--- src/domain/repositories/OrderRepository.ts
+++ src/domain/repositories/OrderRepository.ts
@@ -1,3 +1,15 @@
+import { Order } from "../../../SillyStoreCommon/domain-objects/Order.ts";
+import { OrderProduct } from "../../../SillyStoreCommon/domain-objects/OrderProduct.ts";
+import { Product } from "../../../SillyStoreCommon/domain-objects/Product.ts";
+
 export default interface OrderRepository {
-    /** TODO: stub */
+    getAllAsync(): Promise<Order[] | null>;
+    getByIdAsync(id: number): Promise<Order | null>;
+    getProductsInOrderAsync(id: number): Promise<Product[] | null>;
+    createOrderAsync(dateStr: string): Promise<Order | null>;
+    addProductToOrderAsync({
+        productId,
+        orderId,
+        quantity,
+    }: OrderProduct): Promise<OrderProduct | null>;
 }
--- src/domain/repositories/ProductRepository.ts
+++ src/domain/repositories/ProductRepository.ts
@@ -4,5 +4,5 @@ import { Product } from "../../../SillyStoreCommon/domain-objects/Product.ts";
 export default interface ProductRepository {
     getAllProductsAsync(): Promise<Product[] | null>;
     getProductByIdAsync(id: number): Promise<Product | null>;
-    getOrdersIncludingProduct(productId: number): Promise<Order[] | null>;
+    getOrdersIncludingProductAsync(productId: number): Promise<Order[] | null>;
 }
