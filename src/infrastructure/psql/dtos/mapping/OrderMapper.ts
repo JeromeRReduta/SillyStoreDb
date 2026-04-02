7f7bf4cdec2f6445df332b5ead4523b721f67476
JRReduta <jrreduta@yahoo.com>
2026-04-01 22:36:57 -0700

    (change) ordermapper

--- src/infrastructure/psql/dtos/mapping/OrderMapper.ts
+++ src/infrastructure/psql/dtos/mapping/OrderMapper.ts
@@ -1,1 +1,11 @@
-// TODO
+import { type PgOrder } from "../../entities/PgOrder.ts";
+import { type OrderResponse } from "../responses/OrderResponse.ts";
+import { type PgDtoMapper } from "./PgDtoMapper.ts";
+
+const orderMapper: PgDtoMapper<PgOrder, OrderResponse> = {
+    apply({ id, date, user_id }: PgOrder): OrderResponse {
+        return { id, dateStr: date, userId: user_id };
+    },
+};
+
+export default orderMapper;
