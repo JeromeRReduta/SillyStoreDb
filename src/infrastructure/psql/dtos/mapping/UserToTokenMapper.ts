8049af07b22811b75b5b2202d9b9bcaf520628a1
JRReduta <jrreduta@yahoo.com>
2026-04-01 22:39:16 -0700

    (change) userToTokenMapper - todo: add jwt funcs

--- src/infrastructure/psql/dtos/mapping/UserToTokenMapper.ts
+++ src/infrastructure/psql/dtos/mapping/UserToTokenMapper.ts
@@ -1,1 +1,9 @@
-// TODO
+import { type PgUser } from "../../entities/PgUser.ts";
+import { type TokenResponse } from "../responses/TokenResponse.ts";
+import { type PgDtoMapper } from "./PgDtoMapper.ts";
+
+const userToTokenMapper: PgDtoMapper<PgUser, TokenResponse> = {
+    apply({ id }: PgUser): TokenResponse {
+        return "TODO"; // TODO: add jwt funcs
+    },
+};
