7ebeb030701070b4eb2b381c0b405d095eec386c
JRReduta <jrreduta@yahoo.com>
2026-04-01 22:42:42 -0700

    (change) product mapper

--- src/infrastructure/psql/dtos/mapping/ProductMapper.ts
+++ src/infrastructure/psql/dtos/mapping/ProductMapper.ts
@@ -1,1 +1,15 @@
-// TODO
+import { type PgProduct } from "../../entities/PgProduct.ts";
+import { type ProductResponse } from "../responses/ProductResponse.ts";
+import { type PgDtoMapper } from "./PgDtoMapper.ts";
+
+const productMapper: PgDtoMapper<PgProduct, ProductResponse> = {
+    apply({
+        id,
+        title,
+        description,
+        price,
+        image_src,
+    }: PgProduct): ProductResponse {
+        return { id, title, description, price, imageSrc: image_src };
+    },
+};
