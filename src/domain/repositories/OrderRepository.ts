ac21872c089e20b1c9a84ce39a6e5d886f405218
JRReduta <jrreduta@yahoo.com>
2026-04-02 14:53:31 -0700

    (change) orderrepo now doesn't involve orderproduct, as that's a db concern

--- src/domain/repositories/OrderRepository.ts
+++ src/domain/repositories/OrderRepository.ts
@@ -1,5 +1,4 @@
 import { type Order } from "../../../SillyStoreCommon/domain-objects/Order.ts";
-import { type OrderProduct } from "../../../SillyStoreCommon/domain-objects/OrderProduct.ts";
 import { type Product } from "../../../SillyStoreCommon/domain-objects/Product.ts";
 
 export default interface OrderRepository {
@@ -7,9 +6,4 @@ export default interface OrderRepository {
     getByIdAsync(id: number): Promise<Order | null>;
     getProductsInOrderAsync(id: number): Promise<Product[] | null>;
     createAsync(dateStr: string): Promise<Order | null>;
-    addProductToOrderAsync({
-        productId,
-        orderId,
-        quantity,
-    }: OrderProduct): Promise<OrderProduct | null>;
 }
