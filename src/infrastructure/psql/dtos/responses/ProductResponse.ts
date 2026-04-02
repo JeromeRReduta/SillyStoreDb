424e47116343bd593e4d9d767f1a866ac0c3eba1
JRReduta <jrreduta@yahoo.com>
2026-04-01 22:18:04 -0700

    (change) ProductResponse

--- src/infrastructure/psql/dtos/responses/ProductResponse.ts
+++ src/infrastructure/psql/dtos/responses/ProductResponse.ts
@@ -1,1 +1,7 @@
-// TODO: stub
+export interface ProductResponse {
+    readonly id: number;
+    readonly imageSrc: string;
+    readonly title: string;
+    readonly description: string;
+    readonly price: number;
+}
