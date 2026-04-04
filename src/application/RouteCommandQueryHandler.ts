ae5eeddb3d67e1aeb07a6feb0558df9be641a294
JRReduta <jrreduta@yahoo.com>
2026-04-03 22:21:44 -0700

    (change) RouteCommandQueryHandler

--- src/application/RouteCommandQueryHandler.ts
+++ src/application/RouteCommandQueryHandler.ts
@@ -3,8 +3,11 @@ import { Request } from "express";
 /**
  * Interface for handlers for a command/query. Note that the promise
  * type is Promise\<void\>, as results must be saved in res.locals or req.session (and maybe app.locals but we dont support that yet)
+ *
+ * @type {_TResponse} type of response from repository call
  */
-export interface RouteCommandQueryHandler {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export interface RouteCommandQueryHandler<_TResponse> {
     /**
      * Runs all logging, validation, and repository calls. To save any value from here, save it in
      * res.locals/req.session for per-request/per-session data storage, respectively
