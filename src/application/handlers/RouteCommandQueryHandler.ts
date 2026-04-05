b6226ec97fe5148762b0d997042b2c7a9689be55
JRReduta <jrreduta@yahoo.com>
2026-04-04 17:13:24 -0700

    (change) generic handler

---
+++ src/application/handlers/RouteCommandQueryHandler.ts
@@ -0,0 +1,19 @@
+import type { NextFunction, Request, Response } from "express";
+
+/**
+ * Generic interface for any command/query handler
+ * @type {TRequestParams} what request params should have
+ * @type {TRequestBody} what request body should have
+ * @type {TResponseBody} what response body should have
+ */
+export interface RouteCommandQueryHandler<
+    TRequestParams,
+    TRequestBody,
+    TResponseBody,
+> {
+    handleAsync(
+        req: Request<TRequestParams, TResponseBody, TRequestBody>,
+        res: Response<TResponseBody>,
+        next: NextFunction,
+    ): Promise<void>;
+}
