7faeca60774e53afd59b177c9d7b64df5201b874
JRReduta <jrreduta@yahoo.com>
2026-04-06 15:33:04 -0700

    (change) Repo abstraction

--- src/domain/repos/Repository.ts
+++ src/domain/repos/Repository.ts
@@ -1,1 +1,23 @@
-export
+/**
+ * Data access object
+ * @type {_TDbEntity} Specified for data mapping
+ * @type {TResponse}
+ * @type {TCreateRequest}
+ * @type {TGetAllRequest}
+ * @type {TGetRequest}
+ * @type {TDeleteRequest}
+ */
+export interface Repository<
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    _TDbEntity,
+    TResponse,
+    TCreateRequest,
+    TGetAllRequest,
+    TGetRequest,
+    TDeleteRequest,
+> {
+    createAsync(request: TCreateRequest): Promise<TResponse>;
+    getAllAsync(request: TGetAllRequest): Promise<TResponse>;
+    getAsync(request: TGetRequest): Promise<TResponse>;
+    deleteAsync(request: TDeleteRequest): Promise<TResponse>;
+}
