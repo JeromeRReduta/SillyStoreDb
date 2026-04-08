85c704ab3915b451db8222507daa44a800430d87
JRReduta <jrreduta@yahoo.com>
2026-04-07 17:08:28 -0700

    (change) user repo impl

---
+++ src/domain/repos/UserRepository.ts
@@ -0,0 +1,34 @@
+import { ICreateUserRequest } from "../../application/dtos/requests/ICreateUserRequest.ts";
+import { IDeleteUserRequest } from "../../application/dtos/requests/IDeleteUserRequest.ts";
+import { IGetAllUsersRequest } from "../../application/dtos/requests/IGetAllUsersRequest.ts";
+import { IGetUserByCredentialsRequest } from "../../application/dtos/requests/IGetUserByCredentialsRequest.ts";
+import { IGetUserRequest } from "../../application/dtos/requests/IGetUserRequest.ts";
+import { IUserResponse } from "../../application/dtos/responses/IUserResponse.ts";
+import { IUserDao } from "../../infrastructure/psql/data_access/IUserDao.ts";
+import { IUserRepository } from "./IUserRepository.ts";
+
+export default class UserRepository implements IUserRepository {
+    private dao: IUserDao;
+
+    constructor(dao: IUserDao) {
+        this.dao = dao;
+    }
+
+    async createAsync(dto: ICreateUserRequest): Promise<IUserResponse> {
+        return await this.dao.createAsync(dto);
+    }
+    async getAllAsync(dto: IGetAllUsersRequest): Promise<IUserResponse[]> {
+        throw new Error("Method not implemented.");
+    }
+    async getAsync(dto: IGetUserRequest): Promise<IUserResponse | null> {
+        throw new Error("Method not implemented.");
+    }
+    async deleteAsync(dto: IDeleteUserRequest): Promise<IUserResponse | null> {
+        throw new Error("Method not implemented.");
+    }
+    async getByCredentialsAsync(
+        dto: IGetUserByCredentialsRequest,
+    ): Promise<IUserResponse | null> {
+        throw new Error("Method not implemented.");
+    }
+}
