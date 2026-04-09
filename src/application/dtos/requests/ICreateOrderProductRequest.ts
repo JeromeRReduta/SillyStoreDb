f8a920d971628867d281acb3420cf7a95dd43520
JRReduta <jrreduta@yahoo.com>
2026-04-08 18:51:00 -0700

    (info) get orders including product - comments + spacing

--- src/application/dtos/requests/ICreateOrderProductRequest.ts
+++ src/application/dtos/requests/ICreateOrderProductRequest.ts
@@ -2,5 +2,5 @@ export interface ICreateOrderProductRequest {
     readonly orderId: number;
     readonly productId: number;
     readonly quantity: number;
-    readonly userId: number | null;
+    readonly userId: number | null; // for all order product requests, userId = null means ADMIN; i.e. don't bother checking if you own order/orderproduct entry
 }
--- src/infrastructure/psql/data_access/IOrderProductDao.ts
+++ src/infrastructure/psql/data_access/IOrderProductDao.ts
@@ -1,4 +1,12 @@
+import { ICreateOrderProductRequest } from "../../../application/dtos/requests/ICreateOrderProductRequest.ts";
+import { IDeleteOrderProductRequest } from "../../../application/dtos/requests/IDeleteOrderProductRequest.ts";
+import { IGetAllOrderProductsRequest } from "../../../application/dtos/requests/IGetAllOrderProductsRequest.ts";
+import { IGetOrderProductRequest } from "../../../application/dtos/requests/IGetOrderProductRequest.ts";
+import { IGetOrdersIncludingProductRequest } from "../../../application/dtos/requests/IGetOrdersIncludingProductRequest.ts";
+import { IGetProductsInOrderRequest } from "../../../application/dtos/requests/IGetProductsInOrderRequest.ts";
 import { IOrderProductResponse } from "../../../application/dtos/responses/IOrderProductResponse.ts";
+import { IOrderResponse } from "../../../application/dtos/responses/IOrderResponse.ts";
+import { IProductResponse } from "../../../application/dtos/responses/IProductResponse.ts";
 
 export interface IOrderProductDao {
     createAsync(
@@ -8,16 +16,18 @@ export interface IOrderProductDao {
     getAllAsync(
         dto: IGetAllOrderProductsRequest,
     ): Promise<IOrderProductResponse>;
+
     getAsync(dto: IGetOrderProductRequest): Promise<IOrderProductDao | null>;
+
     deleteAsync(
         dto: IDeleteOrderProductRequest,
     ): Promise<IOrderProductResponse>;
-    getOrdersIncludingProductAsync(dto: IGetOrder);
 
-    // createAsync(dto: ICreateProductRequest): Promise<IProductResponse>;
-    // getAllAsync(dto: IGetAllProductsRequest): Promise<IProductResponse[]>;
-    // getAsync(dto: IGetProductRequest): Promise<IProductResponse | null>;
-    // deleteAsync(dto: IDeleteProductRequest): Promise<IProductResponse | null>;
-    // getOrdersIncludingProductAsync(...)
-    // getProductsInOrderAsync(...)
+    getOrdersIncludingProductAsync(
+        dto: IGetOrdersIncludingProductRequest,
+    ): Promise<IOrderResponse[]>;
+
+    getProductsInOrderAsync(
+        dto: IGetProductsInOrderRequest,
+    ): Promise<IProductResponse[]>;
 }
