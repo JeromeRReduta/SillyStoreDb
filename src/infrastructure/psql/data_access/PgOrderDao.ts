6f6d77a39f93c7a0b287db4f4c744cce8989091d
JRReduta <jrreduta@yahoo.com>
2026-04-10 14:14:38 -0700

    (change) get orders - pgorderdao tested and impl for client AND admin

--- src/infrastructure/psql/data_access/PgOrderDao.ts
+++ src/infrastructure/psql/data_access/PgOrderDao.ts
@@ -1,4 +1,4 @@
-import { Client, Pool } from "pg";
+import { Client, Pool, QueryConfig } from "pg";
 import { ICreateOrderRequest } from "../../../application/dtos/requests/ICreateOrderRequest.ts";
 import { IDeleteOrderRequest } from "../../../application/dtos/requests/IDeleteOrderRequest.ts";
 import { IGetAllOrdersRequest } from "../../../application/dtos/requests/IGetAllOrdersRequest.ts";
@@ -7,6 +7,7 @@ import { IOrderResponse } from "../../../application/dtos/responses/IOrderRespon
 import { IOrderDao } from "../../data_access/IOrderDao.ts";
 import { IDataMapper } from "../../../application/data_mapping/DataMapper.ts";
 import { IPgOrder } from "../entities/IPgOrder.ts";
+import logger from "../../../../SillyStoreCommon/logging/Logger.ts";
 
 export default class PgOrderDao implements IOrderDao {
     private db: Client | Pool;
@@ -23,8 +24,21 @@ export default class PgOrderDao implements IOrderDao {
     async createAsync(dto: ICreateOrderRequest): Promise<IOrderResponse> {
         throw new Error("Method not implemented.");
     }
-    async getAllAsync(dto: IGetAllOrdersRequest): Promise<IOrderResponse[]> {
-        throw new Error("Method not implemented.");
+    async getAllAsync({
+        userId,
+    }: IGetAllOrdersRequest): Promise<IOrderResponse[]> {
+        const isClient: boolean = userId !== null;
+        const sql: QueryConfig = {
+            text: `
+                SELECT * FROM orders
+                ${isClient ? "WHERE user_id = $1" : ""}
+            `,
+            values: isClient ? [userId] : [],
+        };
+        logger.debug("sql: ", sql);
+        const { rows } = await this.db.query(sql);
+        logger.debug("result: ", rows);
+        return rows.map(this.dataMapper);
     }
     async getAsync(dto: IGetOrderRequest): Promise<IOrderResponse | null> {
         throw new Error("Method not implemented.");
--- src/server/test.ts
+++ src/server/test.ts
@@ -20,69 +20,58 @@ import cors from "cors";
 import { TokenResponse } from "../application/dtos/responses/TokenResponse.ts";
 import tokenOps from "../application/jwt/TokenOperations.ts";
 import requireSignedIn from "../application/middleware/RequireSignedIn.ts";
+import psqlErrorHandler from "../application/middleware/PsqlErrorHandler.ts";
+import finalErrorHandler from "../application/middleware/FinalErrorHandler.ts";
+import { IOrderDao } from "../infrastructure/data_access/IOrderDao.ts";
+import PgOrderDao from "../infrastructure/psql/data_access/PgOrderDao.ts";
+import { IGetAllOrdersRequest } from "../application/dtos/requests/IGetAllOrdersRequest.ts";
 
 const app = express();
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(cookieParser());
 app.use(
+    express.json(),
+    morgan("dev"),
+    cookieParser(),
     cors({
         origin: ["http://localhost:3000"],
         credentials: true,
     }),
 );
+const pgOrderDao: IOrderDao = new PgOrderDao(db, pgDataMappers.orderMapper);
 
-app.get("/hello", (_, res) => {
-    res.send("Hello Vite + TypeScript!");
+app.route("/orders").get(async (req, res, next) => {
+    const dto: IGetAllOrdersRequest = {
+        userId: 1,
+    };
+    const orders: IOrderResponse[] = await pgOrderDao.getAllAsync(dto);
+    res.status(HttpStatus.OK).send(orders);
 });
+
+app.route("/admin/orders").get(async (req, res, next) => {
+    const dto: IGetAllOrdersRequest = {
+        userId: null,
+    };
+    const orders: IOrderResponse[] = await pgOrderDao.getAllAsync(dto);
+    res.status(HttpStatus.OK).send(orders);
+});
+
+app.use(psqlErrorHandler, finalErrorHandler);
 
 ViteExpress.listen(app, 3000, async () => {
     await db.connect();
     logger.info("Server is listening on port 3000...");
 });
 
-const orderProductDao: IOrderProductDao = new PgOrderProductDao({
-    db,
-    orderMapper: pgDataMappers.orderMapper,
-    orderProductMapper: pgDataMappers.orderProductMapper,
-    productMapper: pgDataMappers.productMapper,
-});
-const productDao: IProductDao = new PgProductDao(
-    db,
-    pgDataMappers.productMapper,
-);
-const productRepo: IProductRepository = new ProductRepository({
-    orderProductDao,
-    productDao,
-});
-
-app.route("/products/:id/orders").get(
-    requireSignedIn,
-    async (req: Request<{ id: string }, object, object>, res, next) => {
-        try {
-            const orders: IOrderResponse[] =
-                await services.clientProductService.getOrdersIncludingProduct({
-                    // todo: make actual route in products - validate w/ requireSignedIn, but DON'T have to validate req.params.id since it's literally guaranteed by express
-                    productId: parseInt(req.params.id),
-                    userId: req.userId!,
-                });
-            res.status(HttpStatus.OK).send(orders);
-        } catch (e) {
-            next(e);
-        }
-    },
-);
-
-app.use("/users", userRouter);
-
-app.route("/users").get((req, res, next) => {
-    logger.debug("cookies", req.cookies);
-    res.status(200).send("a");
-});
-
-app.use((err, req, res, next) => {
-    // TODO - fix error returning
-    logger.error("ERROR IS", err);
-    const code: number = typeof err.code === "number" ? err.code : 500;
-    res.status(code).send(err);
-});
+// const orderProductDao: IOrderProductDao = new PgOrderProductDao({
+//     db,
+//     orderMapper: pgDataMappers.orderMapper,
+//     orderProductMapper: pgDataMappers.orderProductMapper,
+//     productMapper: pgDataMappers.productMapper,
+// });
+// const productDao: IProductDao = new PgProductDao(
+//     db,
+//     pgDataMappers.productMapper,
+// );
+// const productRepo: IProductRepository = new ProductRepository({
+//     orderProductDao,
+//     productDao,
+// });
