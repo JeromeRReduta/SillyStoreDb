7ef42fc64a4aa20ac9656024e52bf227646f0319
JRReduta <jrreduta@yahoo.com>
2026-04-25 01:40:13 -0700

    (change) icrudrepo - might need to refactor to just icrudobj later

--- src/domain/repos/ICrudRepository.ts
+++ src/domain/repos/ICrudRepository.ts
@@ -10,3 +10,18 @@ export interface IGenericRepository<
     getAsync(dto: IGetRequest): Promise<IResponse | null>;
     deleteAsync(dto: IDeleteRequest): Promise<IResponse | null>;
 }
+
+export interface ICrudRepository<
+    TCreateRequest,
+    TGetAllRequest,
+    TGetRequest,
+    TUpdateRequest,
+    TDeleteRequest,
+    TResponse,
+> {
+    createAsync(dto: TCreateRequest): Promise<TResponse>;
+    getAllAsync(dto: TGetAllRequest): Promise<TResponse[]>;
+    getAsync(dto: TGetRequest): Promise<TResponse | null>;
+    updateAsync(dto: TUpdateRequest): Promise<TResponse | null>;
+    deleteAsync(dto: TDeleteRequest): Promise<TResponse | null>;
+}
