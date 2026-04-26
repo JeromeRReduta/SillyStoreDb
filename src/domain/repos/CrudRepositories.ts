6be1708d1ec28f484e85558ee87ab5b330455734
JRReduta <jrreduta@yahoo.com>
2026-04-25 18:47:51 -0700

    (change) crud repo util impl

--- src/domain/repos/CrudRepositories.ts
+++ src/domain/repos/CrudRepositories.ts
@@ -1,11 +1,60 @@
-export default class CrudRepositories {}
+import { ICrudDao } from "../../infrastructure/data_access/ICrudDao.ts";
 
-// createAsync(dto: TCreateRequest): Promise<TResponse>;
+interface CrudRepositoriesInfo<TRequest, TResponse> {
+    readonly dao: ICrudDao<
+        unknown,
+        unknown,
+        unknown,
+        unknown,
+        unknown,
+        TResponse
+    >;
+    readonly dto: TRequest;
+}
 
-// getAllAsync(dto: TGetAllRequest): Promise<TResponse[]>;
+/** Utility methods for CRUD repositories */
+export default class CrudRepositories {
+    static async createAsync<TCreateRequest, TResponse>({
+        dao,
+        dto,
+    }: CrudRepositoriesInfo<TCreateRequest, TResponse>): Promise<TResponse> {
+        return await dao.createAsync(dto);
+    }
 
-// getAsync(dto: TGetRequest): Promise<TResponse | null>;
+    static async getAllAsync<TGetAllRequest, TResponse>({
+        dao,
+        dto,
+    }: CrudRepositoriesInfo<TGetAllRequest, TResponse>): Promise<TResponse[]> {
+        return await dao.getAllAsync(dto);
+    }
 
-// updateAsync(dto: TUpdateRequest): Promise<TResponse | null>;
+    static async getAsync<TGetRequest, TResponse>({
+        dao,
+        dto,
+    }: CrudRepositoriesInfo<
+        TGetRequest,
+        TResponse
+    >): Promise<TResponse | null> {
+        return await dao.getAsync(dto);
+    }
 
-// deleteAsync(dto: TDeleteRequest): Promise<TResponse | null>;
+    static async updateAsync<TUpdateRequest, TResponse>({
+        dao,
+        dto,
+    }: CrudRepositoriesInfo<
+        TUpdateRequest,
+        TResponse
+    >): Promise<TResponse | null> {
+        return await dao.updateAsync(dto);
+    }
+
+    static async deleteAsync<TDeleteRequest, TResponse>({
+        dao,
+        dto,
+    }: CrudRepositoriesInfo<
+        TDeleteRequest,
+        TResponse
+    >): Promise<TResponse | null> {
+        return await dao.deleteAsync(dto);
+    }
+}
