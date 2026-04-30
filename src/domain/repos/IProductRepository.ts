ebb80582487faac8310c6393e998312a4831db05
JRReduta <jrreduta@yahoo.com>
2026-04-30 01:00:09 -0700

    (change) product repo now uses abs base crud repo

--- src/domain/repos/IProductRepository.ts
+++ src/domain/repos/IProductRepository.ts
@@ -1,22 +1,18 @@
-import { ICreateProductRequest } from "../../../SillyStoreCommon/dtos/requests/create-requests/ICreateProductRequest.ts";
-import { IDeleteProductRequest } from "../../../SillyStoreCommon/dtos/requests/delete-requests/IDeleteProductRequest.ts";
-import { IGetAllProductsRequest } from "../../../SillyStoreCommon/dtos/requests/get-requests/IGetAllProductsRequest.ts";
-import { IGetOrdersIncludingProductRequest } from "../../../SillyStoreCommon/dtos/requests/get-requests/IGetOrdersIncludingProductRequest.ts";
-import { IGetProductRequest } from "../../../SillyStoreCommon/dtos/requests/get-requests/IGetProductRequest.ts";
-import { IUpdateProductRequest } from "../../../SillyStoreCommon/dtos/requests/update-requests/IUpdateProductRequest.ts";
-import { IOrderResponse } from "../../../SillyStoreCommon/dtos/responses/IOrderResponse.ts";
-import { IProductResponse } from "../../../SillyStoreCommon/dtos/responses/IProductResponse.ts";
+import {
+    ICreateProductRequest,
+    IGetAllProductsRequest,
+    IGetProductRequest,
+    IUpdateProductRequest,
+    IDeleteProductRequest,
+    IProductResponse,
+} from "../../../SillyStoreCommon/dtos/productDtos.ts";
 import { ICrudRepository } from "./ICrudRepository.ts";
 
-export interface IProductRepository extends ICrudRepository<
+export type IProductRepository = ICrudRepository<
     ICreateProductRequest,
     IGetAllProductsRequest,
     IGetProductRequest,
     IUpdateProductRequest,
     IDeleteProductRequest,
     IProductResponse
-> {
-    getOrdersIncludingProduct(
-        dto: IGetOrdersIncludingProductRequest,
-    ): Promise<IOrderResponse[]>;
-}
+>;
--- src/domain/repos/ProductRepository.ts
+++ src/domain/repos/ProductRepository.ts
@@ -1,69 +1,27 @@
-import { ICreateProductRequest } from "../../../SillyStoreCommon/dtos/requests/create-requests/ICreateProductRequest.ts";
-import { IDeleteProductRequest } from "../../../SillyStoreCommon/dtos/requests/delete-requests/IDeleteProductRequest.ts";
-import { IGetOrdersIncludingProductRequest } from "../../../SillyStoreCommon/dtos/requests/get-requests/IGetOrdersIncludingProductRequest.ts";
-import { IGetProductRequest } from "../../../SillyStoreCommon/dtos/requests/get-requests/IGetProductRequest.ts";
-import { IUpdateProductRequest } from "../../../SillyStoreCommon/dtos/requests/update-requests/IUpdateProductRequest.ts";
-import { IOrderResponse } from "../../../SillyStoreCommon/dtos/responses/IOrderResponse.ts";
-import { IProductResponse } from "../../../SillyStoreCommon/dtos/responses/IProductResponse.ts";
-import { IOrderProductDao } from "../../infrastructure/data_access/ICartItemDao.ts";
+import {
+    ICreateProductRequest,
+    IDeleteProductRequest,
+    IGetAllProductsRequest,
+    IGetProductRequest,
+    IProductResponse,
+    IUpdateProductRequest,
+} from "../../../SillyStoreCommon/dtos/productDtos.ts";
 import { IProductDao } from "../../infrastructure/data_access/IProductDao.ts";
-import CrudRepositories from "./CrudRepositories.ts";
+import AbstractBaseCrudRepository from "./AbstractBaseCrudRepository.ts";
 import { IProductRepository } from "./IProductRepository.ts";
 
-export default class ProductRepository implements IProductRepository {
-    orderProductDao: IOrderProductDao;
-    productDao: IProductDao;
-
-    constructor({
-        orderProductDao,
-        productDao,
-    }: {
-        orderProductDao: IOrderProductDao;
-        productDao: IProductDao;
-    }) {
-        this.orderProductDao = orderProductDao;
-        this.productDao = productDao;
-    }
-
-    async createAsync(dto: ICreateProductRequest): Promise<IProductResponse> {
-        return await CrudRepositories.createAsync({
-            dao: this.productDao,
-            dto,
-        });
-    }
-
-    async getAllAsync(dto: object): Promise<IProductResponse[]> {
-        return await CrudRepositories.getAllAsync({
-            dao: this.productDao,
-            dto,
-        });
-    }
-
-    async getAsync(dto: IGetProductRequest): Promise<IProductResponse | null> {
-        return await CrudRepositories.getAsync({ dao: this.productDao, dto });
-    }
-
-    async updateAsync(
-        dto: IUpdateProductRequest,
-    ): Promise<IProductResponse | null> {
-        return await CrudRepositories.updateAsync({
-            dao: this.productDao,
-            dto,
-        });
-    }
-
-    async deleteAsync(
-        dto: IDeleteProductRequest,
-    ): Promise<IProductResponse | null> {
-        return await CrudRepositories.deleteAsync({
-            dao: this.productDao,
-            dto,
-        });
-    }
-
-    async getOrdersIncludingProduct(
-        dto: IGetOrdersIncludingProductRequest,
-    ): Promise<IOrderResponse[]> {
-        return await this.orderProductDao.getOrdersIncludingProductAsync(dto);
+export default class ProductRepository
+    extends AbstractBaseCrudRepository<
+        ICreateProductRequest,
+        IGetAllProductsRequest,
+        IGetProductRequest,
+        IUpdateProductRequest,
+        IDeleteProductRequest,
+        IProductResponse
+    >
+    implements IProductRepository
+{
+    constructor(dao: IProductDao) {
+        super(dao);
     }
 }
