62a0169cb3f591f93f23695fff621c2a214f16f1
JRReduta <jrreduta@yahoo.com>
2026-04-30 19:00:24 -0700

    (change) fixed some imports - pre-purge commit

--- src/application/services/IOrderClientService.ts
+++ src/application/services/IOrderClientService.ts
@@ -1,16 +1,16 @@
-import { ICreateOrderProductRequest } from "../../../SillyStoreCommon/dtos/requests/create-requests/ICreateOrderProductRequest.ts";
-import { ICreateOrderRequest } from "../../../SillyStoreCommon/dtos/requests/create-requests/ICreateOrderRequest.ts";
-import { IGetAllOrdersRequest } from "../../../SillyStoreCommon/dtos/requests/get-requests/IGetAllOrdersRequest.ts";
-import { IGetAllPendingOrdersRequest } from "../../../SillyStoreCommon/dtos/requests/get-requests/IGetAllPendingOrdersRequest.ts";
-import { IGetOrderRequest } from "../../../SillyStoreCommon/dtos/requests/get-requests/IGetOrderRequest.ts";
-import { IGetProductsInOrderRequest } from "../../../SillyStoreCommon/dtos/requests/get-requests/IGetProductsInOrderRequest.ts";
-import { IOrderProductResponse } from "../../../SillyStoreCommon/dtos/responses/IOrderProductResponse.ts";
-import { IOrderResponse } from "../../../SillyStoreCommon/dtos/responses/IOrderResponse.ts";
-import { IProductResponse } from "../../../SillyStoreCommon/dtos/responses/IProductResponse.ts";
-import { IProductWithQuantityResponse } from "../../../SillyStoreCommon/dtos/responses/IProductWithQuantityResponse.ts";
+import {
+    ICreateOrderRequest,
+    IOrderResponse,
+    IGetAllOrdersRequest,
+    IGetOrderRequest,
+    IGetAllPendingOrdersRequest,
+} from "../../../SillyStoreCommon/dtos/orderDtos.ts";
+import {
+    IProductResponse,
+    IProductWithQuantityResponse,
+} from "../../../SillyStoreCommon/dtos/productDtos.ts";
 
 export interface IClientOrderService {
-    // note: DON'T make generic CrudService, as each service as different things it needs to do
     createAsync(dto: ICreateOrderRequest): Promise<IOrderResponse>;
 
     getAllOwnedAsync(dto: IGetAllOrdersRequest): Promise<IOrderResponse[]>;
